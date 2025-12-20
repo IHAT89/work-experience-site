d56d5fd5191214fcd2ddb0725540b87795311aca
Ian Townrow <iantownrow@gmail.com>
2025-12-20 16:14:47 +0800

    Update frontend: add dashboard preview, fix accessibility, update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/HoneyBeeKissingHeart48x48.png" />
         <meta charSet="UTF-8" />
         <meta name="theme-color" content="#007aff" />
         <meta name="author" content="Work Experience Pte Ltd" />
